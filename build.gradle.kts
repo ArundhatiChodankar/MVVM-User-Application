5af80f215ab2bd7b458d6ba794e59ce8d3eee768
Arundhati <arundhatichodankar10@gmail.com>
2025-02-17 17:07:58 +0530

    dependencies added

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,5 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
 }
+
+buildscript { dependencies { classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.0") } }
